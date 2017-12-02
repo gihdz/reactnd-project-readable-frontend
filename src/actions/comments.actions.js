1c5f2d627d78d0350cf5369e05e8893e67de29c1
ic_hernandez <gdhblok@gmail.com>
2017-12-02 16:00:48 -0400

    First commit

---
+++ src/actions/comments.actions.js
@@ -0,0 +1,26 @@
+import * as actions from '../constants';
+
+export const getComments = () => {
+  return {
+    type: actions.GET_COMMENTS,
+    comments: [
+      { id: 1, text: 'first' },
+      { id: 2, text: 'second' },
+      { id: 3, text: 'third' }
+    ]
+  };
+};
+
+export const getAsyncComments = (dispatch, getState) => {
+  return () => {
+    console.log('mira manin, soy un estado', getState());
+    dispatch({
+      type: actions.GET_COMMENTS,
+      comments: [
+        { id: 1, text: 'first' },
+        { id: 2, text: 'second' },
+        { id: 3, text: 'third' }
+      ]
+    });
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers/';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
+import './index.css';
+
+const initialState = {
+  comments: []
+};
+
+const middlewares = [thunk];
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  initialState,
+  composeEnhancer(applyMiddleware(...middlewares))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
